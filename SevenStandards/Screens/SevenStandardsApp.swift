a9483fa8215a9431807ac03031812a82cb842d85
Miller <eotjd8721@naver.com>
2022-06-01 16:21:50 +0900

    Initial setting

---
+++ SevenStandards/Screens/SevenStandardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SevenStandardsApp.swift
+//  SevenStandards
+//
+//  Created by DaeSeong on 2022/06/01.
+//
+
+import SwiftUI
+
+@main
+struct SevenStandardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
